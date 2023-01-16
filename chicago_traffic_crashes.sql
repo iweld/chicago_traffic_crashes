9046eb3a1c79a29a21a8348e47156a592a442f96
Jaime M. Shaker <eyeweld@gmail.com>
2023-01-16 04:37:08 -0600

    Adding sql query file

---
+++ chicago_traffic_crashes.sql
@@ -0,0 +1,14 @@
+/*
+
+	Chicago Traffic Crashes
+	
+	An SQL analysis about each traffic crash on city streets within the City of Chicago limits and under 
+	the jurisdiction of Chicago Police Department (CPD). Data shown as is from the electronic crash reporting system 
+	(E-Crash) at CPD, excluding any personally identifiable information.
+	
+	Author: Jaime M. Shaker
+	Email: jaime.m.shaker@gmail.com
+	Website: https://www.shaker.dev
+	
+
+*/
