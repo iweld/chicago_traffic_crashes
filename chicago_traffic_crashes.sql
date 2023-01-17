d106bc144ae068372ace4d902be7825bc7b3f7ea
Jaime M. Shaker <eyeweld@gmail.com>
2023-01-16 20:28:59 -0600

    Adding crash count query between 2017 and 2022 and weather condition

--- chicago_traffic_crashes.sql
+++ chicago_traffic_crashes.sql
@@ -109,30 +109,55 @@ crash_year|reported_crashes|
     2022.0|          108292|
     2023.0|            2509|
  
--- What is the number of reported crashes 2017 and 2022?   
+-- What is the number of reported crashes and weather conditions betweeen 2017 and 2022?   
 
 SELECT
 	EXTRACT(YEAR FROM crash_date) AS crash_year,
+	weather_condition,
 	count(*) AS reported_crashes
 FROM
 	crashes
 WHERE
 	EXTRACT(YEAR FROM crash_date) between '2017.0' AND '2022.0'
+AND
+	weather_condition != 'unknown'
 GROUP BY
-	crash_year
+	crash_year,
+	weather_condition
+HAVING count(*) > 1999
 ORDER BY 
 	crash_year;
 
 -- Results:
 
-crash_year|reported_crashes|
-----------+----------------+
-    2017.0|           83786|
-    2018.0|          118950|
-    2019.0|          117762|
-    2020.0|           92088|
-    2021.0|          108756|
-    2022.0|          108292|
+crash_year|weather_condition|reported_crashes|
+----------+-----------------+----------------+
+    2017.0|clear            |           67663|
+    2017.0|cloudy/overcast  |            2310|
+    2017.0|rain             |            8302|
+    2017.0|snow             |            2011|
+    2018.0|clear            |           94006|
+    2018.0|cloudy/overcast  |            3853|
+    2018.0|rain             |           10744|
+    2018.0|snow             |            4621|
+    2019.0|clear            |           91604|
+    2019.0|cloudy/overcast  |            3885|
+    2019.0|rain             |           11305|
+    2019.0|snow             |            4893|
+    2020.0|clear            |           74195|
+    2020.0|cloudy/overcast  |            2509|
+    2020.0|rain             |            7626|
+    2020.0|snow             |            3042|
+    2021.0|clear            |           86696|
+    2021.0|cloudy/overcast  |            3180|
+    2021.0|rain             |            7825|
+    2021.0|snow             |            4323|
+    2022.0|clear            |           83615|
+    2022.0|cloudy/overcast  |            2966|
+    2022.0|rain             |            8626|
+    2022.0|snow             |            4195|
+
+    
 
 
 
