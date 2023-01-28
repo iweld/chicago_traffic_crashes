da5ed3dfc7b4b611f6140413f1af81d90b189eb6
Jaime M. Shaker <eyeweld@gmail.com>
2023-01-28 08:57:25 -0600

    Adding new crash analysis file

---
+++ traffic_crash_analysis.sql
@@ -0,0 +1,56 @@
+/*
+
+	Chicago Traffic Crashes
+	
+	An SQL analysis about each traffic crash on city streets within the City of Chicago limits and under 
+	the jurisdiction of Chicago Police Department (CPD). Data shown as is from the electronic crash reporting system 
+	(E-Crash) at CPD, excluding any personally identifiable information.
+	
+	Author: Jaime M. Shaker
+	Email: jaime.m.shaker@gmail.com
+	Website: https://www.shaker.dev
+	
+		column_name              |
+	-----------------------------+
+	crash_date                   |
+	date_police_notified         |
+	crash_id                     |
+	device_condition             |
+	weather_condition            |
+	lighting_condition           |
+	first_crash_type             |
+	traffic_way_type             |
+	lane_count                   |
+	alignment                    |
+	roadway_surface_condition    |
+	road_defect                  |
+	report_type                  |
+	crash_type                   |
+	hit_and_run                  |
+	damage                       |
+	primary_cause                |
+	secondary_cause              |
+	street_direction             |
+	street_name                  |
+	statement_taken              |
+	work_zone                    |
+	work_zone_type               |
+	workers_present              |
+	number_unit                  |
+	most_severe_injury           |
+	injuries_total               |
+	injuries_fatal               |
+	injuries_incapacitated       |
+	injuries_non_incapacitated   |
+	injuries_reported_not_evident|
+	crash_hour                   |
+	crash_day_of_week            |
+	crash_month                  |
+	latitude                     |
+	longitude                    |
+	crash_location               |
+	posted_speed_limit           |
+	traffic_control_device       |
+
+*/
+
